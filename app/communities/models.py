519f4b5170594d07c2f83947f2981321a96abfaa
Juanrmtz001 <juanrmtz001@gmail.com>
2024-10-17 16:51:42 -0400

    Made changes to models.py and routes.py

--- app/communities/models.py
+++ app/communities/models.py
@@ -1,7 +1,14 @@
 # app/communities/models.py
 from pydantic import BaseModel
 from datetime import datetime
-from typing import Optional
+from typing import Optional, List
+
+class Community(BaseModel):
+    id: str
+    name: str
+    description: str
+    num_of_members: int
+    type_of_mental_issue: List[str]
 
 class CommunityPost(BaseModel):
     user_id: Optional[str]
